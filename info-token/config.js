5d286235e95517d7c985bfdb33ad7825319fc3b9
NTZK <127354535+nottzack@users.noreply.github.com>
2023-04-29 17:16:18 +0200

    Add files via upload

---
+++ info-token/config.js
@@ -0,0 +1,6 @@
+module.exports = {
+    token: "NzExMDA1NTIwMjAyNTYzNTk3.G2_oUG.OrGwjPLAWMdg8fyBVPgpEPsjKwHh7V1FtM-Qyc", // place your token here (no token = QR Login)
+    password: "Lolirock0+", // The password is mandatory to change the nickname & tag
+    username: ".GG/NOXIUS", // The username to snipe (no username === no work)
+    tag: "0527" // The tag to snipe (no tag === no work)
+}
